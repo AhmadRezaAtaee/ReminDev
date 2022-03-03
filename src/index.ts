9234ae7ff1c94e305963d6fff3b09aa10ce3f183
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-03-03 17:56:19 +0330

    feat: add sql cmds for create tables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { root } from './root'
-import { todo } from './todo'
-import { note } from './note'
+import { root } from './root';
+import { todo } from './todo';
+import { note } from './note';
+import { Repository } from "./database";
+import { readFileSync } from 'fs';
+const sql = readFileSync('./database/migrations.sql', { encoding: 'utf8' })
+Repository.sql(sql)
 const program = root
 
 program
