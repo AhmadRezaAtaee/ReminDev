74fa37e6fc10df1b349bc60fe2490caedc9575fb
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-03-03 18:02:24 +0330

    feat: add create todo command

--- src/todo/index.ts
+++ src/todo/index.ts
@@ -1,3 +1,33 @@
 import { Command } from "commander";
+import { Todo } from "./repository";
+import * as inquirer from "inquirer"
 
 export const todo = new Command('todo')
+
+const add = new Command('add')
+	.argument('<title>')
+	.argument('[content]')
+	.option('-e, --editor')
+	.action(async (title, content, { editor }) => {
+		const res = Todo.insert({
+			title: title || 'no title',
+			content,
+			created_at: new Date(Date.now()).toISOString(),
+		})
+		console.log(res)
+	})
+
+const get = new Command('list')
+	.action(async () => { })
+
+const update = new Command('edit')
+	.action(async () => { })
+
+const remove = new Command('delete')
+	.action(async () => { })
+
+todo
+	.addCommand(add)
+	.addCommand(remove)
+	.addCommand(update)
+	.addCommand(get)
