773b4b28a08f14d3a010d7406adcf44b1a228ee2
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-03-03 17:56:19 +0330

    fix: db path

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,3 @@
 import * as sqlite from 'better-sqlite3';
 
-export const database: sqlite.Database = new sqlite('./db.db')
+export const database: sqlite.Database = new sqlite(process.env.DB_PATH || 'remindev.db')
